6f2493126bf99e933012e460e862541723ce9fd0
EdlawitG <eduedil30@gmail.com>
2023-12-17 11:12:32 +0300

    first commit

---
+++ ladingpage/src/App.jsx
@@ -0,0 +1,120 @@
+import "./App.css";
+
+import Sidebar from './Sidebar.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{position: 'fixed', right: '50%', bottom: '0px', color: 'aqua'}}>@copyright kept</h1>
+       {/* <Sidebar side="left" /> */}
+      <div>
+        <div style={{ backgroundColor: "rgb(255, 10, 103)", marginBottom: '20px'}}>
+          <img
+            src={
+              "https://cdn.openart.ai/stable_diffusion/6a107fdd6a3bf8eaa1c235f8e123ce9242da51f9_2000x2000.webp"
+            }
+            style={{ width: "300px", height: "300px" }}
+          />
+          <h3 style={{display:"inline"}}>
+            "Picture perfect, you don't need no filter Gorgeous, make 'em drop
+            dead, you a killer Shower you with all my attention Yeah, these are
+            my only intentions Stay in the kitchen cookin' up, got your own
+            bread Heart full of equity, you're an asset Make sure that you don't
+            need no mentions Yeah, these are my only intentions Shout-out to
+            your mom and dad for makin' you Standin' ovation, they did a great
+            job raisin' you When I create, you're my muse That kind of smile
+            that makes the news Can't nobody throw shade on your name in these
+            streets Triple threat, you a boss, you a bae, you a beast You make
+            it easy to choose You got a mean touch, I can't refuse (No, I can't
+            refuse it)" <h2 style={{display:"inline"}}>damnnnnnnn this music is for me .</h2>
+          </h3>
+          <button style={{padding: '20px',fontSize:'20px', fontWeight:'700', backgroundColor: 'yellow',}} className="blinking-button">you can call 911 if you want to hang out</button>
+        </div>
+        <div style={{display:"flex"}}>
+        <div style={{ backgroundColor: "rgb(100, 200, 53)", marginBottom: '20px'}}>
+
+          <img
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI9tTnBJvkM991HkCNh9T_5DW2O1FP9Oncew&usqp=CAU"
+            }
+            style={{ width: "300px", height: "300px" }}
+          />
+          <h3 style={{fontSize: '40px', fontWeight: '800'}}>
+          some say am gorgeoues some says am beautifull like hell but i don't believe them until some one like you confirm it dagi
+          </h3>
+          <button style={{padding: '20px',fontSize:'20px', fontWeight:'700', backgroundColor: 'orange'}} className="hover-button">cAll mE or mISs YoUR cHaNcE<br /></button>
+        </div>
+        <div style={{ backgroundColor: "rgb(100, 200, 53)", marginBottom: '20px'}}>
+         
+          <h3 style={{display:"inline"}}>
+          "From the moment I met you, I was captivated by your radiant smile that lights up the room and the warmth that emanates from your presence. Your inner beauty shines even brighter than the sun, and it's a privilege to witness your grace and elegance.
+
+Your kind heart and compassionate nature are truly remarkable. You have an uncanny ability to make everyone around you feel valued and appreciated. Your genuine care and empathy touch the lives of those fortunate enough to know you, leaving a lasting impact that words can hardly express.
+
+But it's not just your kindness that sets you apart; your intelligence and wisdom are awe-inspiring. Your insightful perspective and thoughtful words always offer a fresh and enlightening outlook. You possess a rare combination of intelligence, creativity, and curiosity that continuously fuels your thirst for knowledge and growth.
+
+Your inner strength is truly admirable. You face challenges head-on with unwavering determination, never allowing adversity to dim your spirit. Your resilience and courage inspire those around you to push beyond their limits and reach for their own dreams.
+
+Beyond your captivating personality, your physical beauty is simply breathtaking. Your eyes, like sparkling gems, hold a world of stories and emotions. Your smile, so captivating and genuine, has the power to brighten the darkest of days. Your presence commands attention and leaves an indelible impression on all who cross your path.
+
+But perhaps what makes you most beautiful is your authenticity. You embrace your true self and radiate confidence in every step you take. Your uniqueness is a beacon of light in a world that often tries to mold individuals into conformity. You remind us all that it's our differences that make us truly special.
+
+In the tapestry of life, you are a masterpiece—an exquisite combination of strength, kindness, intelligence, and beauty. Your presence makes the world a more beautiful place, and I am grateful to know you. Never forget the incredible impact you have on others and how deeply cherished you are." 
+          </h3>
+          <img
+            src={
+              "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/04/02/19/Anne-Woods.jpg"
+            }
+            style={{ width: "300px", height: "300px", position: 'absolute'}}
+          />
+          <h5  style={{ position: 'absolute'}}> I found myself compelled to share a heartbreaking tale of love. With a heavy heart and a desire to connect with others who might have experienced similar struggles, I crafted a post that would lay bare the painful chapters of my past.
+
+As I typed the words, my fingers trembled with a mix of vulnerability and anticipation. I recounted a love story that began with hope but unfortunately took a dark turn, leaving me with scars that were invisible to the world. The anonymity of the platform provided a safe space for me to share my truth without revealing my identity.
+
+In my post, I described an encounter with someone I referred to as "M." M was a captivating figure who initially seemed like a dream come true. We shared beautiful moments together, filled with laughter, passion, and promises of a future together. I believed I had found my soulmate—the one who would complete the missing pieces of my heart.
+
+However, as time went on, M's true colors began to emerge. Their affection and care transformed into manipulation and control. I detailed the emotional abuse I endured—the constant belittling, gaslighting, and isolation from loved ones. The love that once brought me joy became a daily nightmare, leaving me shattered and questioning my own worth.
+
+Through my anonymous post, I reached out for support, seeking solace and understanding from the online community. I shared the nights spent in tears, the loneliness that enveloped me, and the fear that consumed my every thought. I hoped that by sharing my story, I would find empathy, encouragement, and perhaps even guidance from those who had walked a similar path.
+
+As the post went live, I felt a mix of relief and trepidation. The weight of my story was finally lifted from my shoulders, and I knew that no matter the response, I had taken a brave step towards healing. In the virtual space, I found a community of anonymous voices who shared their own stories, offering support, empathy, and words of wisdom.
+
+Through the collective strength of our digital connections, we began to heal together, supporting one another as we embarked on our individual journeys of recovery and self-love. The anonymous nature of the platform allowed us to bond over shared experiences, offering a sense of validation and understanding that we had longed for.
+
+In that virtual realm, I discovered that while the pain of my love story was real, so too was the resilience and empathy of others who had faced similar struggles. And as I continued to navigate the path of healing, I knew that my anonymous post had not only touched hearts but also helped shed light on the silent battles fought by many.</h5>
+           <button style={{padding: '20px', margin: '10px',fontSize:'20px', fontWeight:'700', backgroundColor: 'red',}} className="blinking-button">date me with high status</button>
+          <button style={{padding: '20px', margin: '50px',fontSize:'20px', fontWeight:'700', backgroundColor: 'blue',}} className="blinking-button">let's start our LOVE journey</button>
+          <button style={{padding: '20px', margin: '70px',fontSize:'20px', fontWeight:'700', backgroundColor: 'black', color: 'brown'}} className="blinking-button">call any time @cutie97</button>
+          <button style={{padding: '20px', margin: '20px',fontSize:'20px', fontWeight:'700', backgroundColor: 'gray',}} className="blinking-button">get me at street N702</button>
+          <button style={{padding: '20px',fontSize:'20px', fontWeight:'700', backgroundColor: 'green',}} className="blinking-button">you can get my number from my EX</button>
+         
+          <img
+            src={
+              "https://i.pinimg.com/564x/45/04/82/45048273dc38a775dfca17d0315cbbec.jpg"
+            }
+            style={{ width: "300px", height: "300px" }}
+          />
+          <button style={{padding: '20px',fontSize:'20px', fontWeight:'700', backgroundColor: 'lime',}} className="blinking-button">Let's meat up</button>
+        </div>
+        </div>
+        <h1
+          style={{
+            fontFamily: "Kablammo, sans-serif",
+            fontSize: "200px",
+            position: "absolute",
+            top: "-40px",
+            left:"250px",
+            zIndex: "1000",
+          }}
+        >
+          The Find Your True Love 🖤
+        </h1>
+       
+     
+      </div>
+      <Sidebar side="right" />
+    </div>
+  );
+}
+
+export default App;
